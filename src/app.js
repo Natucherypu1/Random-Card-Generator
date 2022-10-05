64f4cff0ae4521bd4d955e43407c345ab0f0fd68
Natucherypu1 <nathalie.chery@gmail.com>
2022-10-05 19:00:17 +0000

    started Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["heart", "spade", "club", "diamond"];
+  let rank = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  function getRandomInt(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  console.log(suits[getRandomInt(suits)]);
+  console.log(rank[getRandomInt(rank)]);
 };

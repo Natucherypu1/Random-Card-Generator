36b91b94615c88796b0055f2138037ad2bebc625
Natucherypu1 <nathalie.chery@gmail.com>
2022-10-06 22:36:33 +0000

    Finished Project

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let suits = ["heart", "spade", "club", "diamond"];
+  let suits = ["hearts", "spades", "clubs", "diamonds"];
   let rank = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   function getRandomInt(array) {
@@ -15,4 +15,9 @@ window.onload = function() {
   }
   console.log(suits[getRandomInt(suits)]);
   console.log(rank[getRandomInt(rank)]);
+  let card = document.querySelector(".card");
+  let rankDiv = document.querySelector(".rank");
+
+  card.classList.add(suits[getRandomInt(suits)]);
+  rankDiv.innerHTML = rank[getRandomInt(rank)];
 };
